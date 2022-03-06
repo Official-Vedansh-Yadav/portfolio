594ea1eee508145bbd7115b38b69363f76445cc7
Vedansh_yadav <83397824+Official-Vedansh-Yadav@users.noreply.github.com>
2022-03-06 20:30:43 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,26 @@ $('.slider').slick({
     appendDots: '.slider-dots',
     dotsClass: 'dots',
 });
+
+let hamberger = document.querySelector('.hamberger');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+let mobileNavLinks = document.querySelectorAll('.mobile-nav-links');
+
+
+hamberger.addEventListener('click', function(){
+    mobileNav.classList.add('open');
+})
+
+times.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+})
+
+// Function to close mobile nav when user click on any link
+
+for (let i = 0; i < 4; i++) {
+    const element = mobileNavLinks[i];
+    element.addEventListener('click', ()=>{
+        mobileNav.classList.remove('open');
+    })
+}
